bd0d3c8716592539ed2cabe10966d57123ceff21
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-03-03 23:25:09 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String n = br.readLine();
+        br.close();
+
+        char[] arr = n.toCharArray();
+
+        // 버블 정렬
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    char temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(new String(arr));
+        br.close();
+    }
+}
