4ff3a3f91d479b1ae8ea21d8db190c727d13f99e
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-03-15 22:22:11 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 476 ms, Memory: 60392 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(st.nextToken()); // 포켓몬 번호
         int M = Integer.parseInt(st.nextToken()); // 문제의 개수
@@ -21,8 +22,9 @@ public class Main {
         }
         for (int i = 0; i < M; i++) {
             String s = br.readLine();
-            if (nameToIndex.containsKey(s)) System.out.println(nameToIndex.get(s));
-            else System.out.println(indexToname.get(Integer.parseInt(s)));
+            if (nameToIndex.containsKey(s)) sb.append(nameToIndex.get(s)).append('\n');
+            else sb.append(indexToname.get(Integer.parseInt(s))).append('\n');
         }
+        System.out.println(sb);
     }
 }
