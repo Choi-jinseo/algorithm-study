c19e74c68cd87ae536bb1a153beab2e9901149db
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-03-24 16:02:48 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 124 ms, Memory: 14800 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            arr.add(i);
+        }
+
+        int cursor = K-1;
+        for (int i = 0; i < N; i++) {
+            if (i == 0) sb.append("<");
+            if (i == N-1) {
+                sb.append(arr.get(0)).append(">");
+                break;
+            }
+            if (cursor < arr.size()) {
+                sb.append(arr.get(cursor)).append(", ");
+                arr.remove(cursor);
+                cursor--;
+            }
+            else {
+                cursor = cursor % arr.size();
+                sb.append(arr.get(cursor)).append(", ");
+                arr.remove(cursor);
+                cursor--;
+            }
+            cursor += K;
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
