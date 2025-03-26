c985567a0dc0e711294d166ead0aa51b9f7dc309
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-03-26 09:37:47 +0900

    [Silver IV] Title: 베스트셀러, Time: 104 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine()); // 큐의 크기
+
+        HashMap<String, Integer> bookCount = new HashMap<>();
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            if (!bookCount.containsKey(str)) bookCount.put(str, 1);
+            else bookCount.put(str, bookCount.get(str) + 1);
+        }
+
+        int max = 0;
+        TreeMap<String, Integer> tm = new TreeMap<>();
+        for (String key : bookCount.keySet()) {
+            if (bookCount.get(key) == max) tm.put(key, 0);
+            else if (bookCount.get(key) > max) {
+                max = bookCount.get(key);
+                tm.clear();
+                tm.put(key, 0);
+            }
+        }
+
+        for (String key : tm.keySet()) {
+            System.out.println(key);
+            break;
+        }
+
+        br.close();
+    }
+}
