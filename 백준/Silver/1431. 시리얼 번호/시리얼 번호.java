e2b81b68457eafbc7a8aa49fcb8c28e20e27528e
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-04-03 20:34:20 +0900

    [Silver III] Title: 시리얼 번호, Time: 136 ms, Memory: 16168 KB -BaekjoonHub

--- 백준/Silver/1431. 시리얼 번호/시리얼 번호.java
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.java
@@ -6,21 +6,20 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
         String[] str = new String[N];
-        // 짧은 문자
-        // 자릿수합이 적은 문자
-        // 사전순 (숫자가 더 작음)
+
         for (int i = 0; i < N; i++) {
             str[i] = br.readLine();
         }
         for (int i = 1; i < N; i++) { // 버블 정렬
             for (int j = 0; j < N - i; j++) {
-                if (str[j].length() > str[j+1].length()) {
+                if (str[j].length() > str[j+1].length()) { // 문장 길이
                     swap(str, j, j + 1);
                 }
-                else if (str[j].length() == str[j+1].length()) {
+                else if (str[j].length() == str[j+1].length()) { // 자릿수 합
                     int cnt1 = 0;
                     int cnt2 = 0;
                     for (int k = 0; k < str[j].length(); k++) {
@@ -30,7 +29,7 @@ public class Main {
                     if (cnt1 > cnt2) {
                         swap(str, j, j + 1);
                     }
-                    else if (cnt1 == cnt2) {
+                    else if (cnt1 == cnt2) { // 사전순
                         for (int k = 0; k < str[j].length(); k++) {
                             if (str[j].charAt(k) >= '0' && str[j].charAt(k) <= '9') cnt1 = str[j].charAt(k) - '0';
                             else cnt1 = str[j].charAt(k) - 'A' + 10;
@@ -47,9 +46,9 @@ public class Main {
             }
         }
         for (int i = 0; i < N; i++) {
-            System.out.println(str[i]);
+            sb.append(str[i]).append('\n');
         }
-
+        System.out.print(sb);
         br.close();
     }
     static void swap(String[] str, int i, int j) {
