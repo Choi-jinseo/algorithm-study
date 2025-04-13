fe63ea212a7cf5f26daddea204dba6cf47a4bcb3
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-04-13 22:26:44 +0900

    [Silver IV] Title: 카드, Time: 140 ms, Memory: 26176 KB -BaekjoonHub

---
+++ 백준/Silver/1835. 카드/카드.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> deque = new LinkedList<>();
+
+        deque.addFirst(N);
+
+        for (int i = N - 1; i > 0; i--) {
+            deque.addFirst(i);
+            for (int j = 0; j < i; j++) {
+                deque.addFirst(deque.removeLast());
+            }
+        }
+        for (Integer i: deque) {
+            sb.append(i).append(" ");
+        }
+        System.out.println(sb);
+
+        br.close();
+    }
+}
