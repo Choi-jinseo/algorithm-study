06cf2b7c8b2ee095be172d02ac0feaf02ec98dfa
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-04-24 18:05:27 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static boolean[][] visited;
+    static String[] map;
+    static int count;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        visited = new boolean[N][N];
+        map = new String[N];
+        for (int i = 0; i < N; i++) {
+            map[i] = br.readLine();
+        }
+        ArrayList<Integer> counts = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i].charAt(j) == '1' && !visited[i][j]) {
+                    count = 0;
+                    dfs(i, j, N);
+                    counts.add(count);
+                }
+            }
+        }
+        counts.sort(Comparator.naturalOrder());
+        sb.append(counts.size()).append('\n');
+        for (Integer count : counts) {
+            sb.append(count).append('\n');
+        }
+        System.out.print(sb);
+        br.close();
+    }
+    static void dfs(int i, int j, int N){
+        visited[i][j] = true;
+        count++;
+        if (i-1 >= 0 && !visited[i-1][j] && map[i-1].charAt(j) == '1') dfs(i-1, j, N);
+        if (i+1 < N && !visited[i+1][j] && map[i+1].charAt(j) == '1') dfs(i+1, j, N);
+        if (j-1 >= 0 && !visited[i][j-1] && map[i].charAt(j-1) == '1') dfs(i, j-1, N);
+        if (j+1 < N && !visited[i][j+1] && map[i].charAt(j+1) == '1') dfs(i, j+1, N);
+    }
+}
