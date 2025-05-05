2aac655456b8b091720ce36f58aa258be4f0aa13
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-05-05 14:07:38 +0900

    [Silver V] Title: 점수 계산, Time: 108 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/2822. 점수 계산/점수 계산.java
+++ 백준/Silver/2822. 점수 계산/점수 계산.java
@@ -1,47 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder();
         int[] arr = new int[8];
-        int[] idx = new int[8];
+        ArrayList<Integer> prob = new ArrayList<>();
         for (int i = 0; i < 8; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            idx[i] = i + 1;
+            int num = Integer.parseInt(br.readLine());
+            arr[i] = num;
+            prob.add(num);
         }
-        for (int i = 0; i < 8; i++) {
-            for (int j = 1; j < 8 - i; j++) {
-                if (arr[j-1] < arr[j]) {
-                    int arrTmp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = arrTmp;
-
-                    int idxTmp = idx[j-1];
-                    idx[j-1] = idx[j];
-                    idx[j] = idxTmp;
-                }
-            }
-        }
-        for (int i = 0; i < 5; i++) {
-            for (int j = 1; j < 5-i; j++) {
-                if (idx[j-1] > idx[j]) {
-                    int idxTmp = idx[j-1];
-                    idx[j-1] = idx[j];
-                    idx[j] = idxTmp;
-                }
-            }
-        }
+        Collections.sort(prob);
+        int temp = prob.get(2);
         int count = 0;
-        for (int i = 0; i < 5; i++) {
-            count += arr[i];
+        for (int i = 0; i < 8; i++) {
+            if (arr[i] > temp) {
+                count += arr[i];
+                sb.append(i + 1).append(" ");
+            }
         }
         System.out.println(count);
-        for (int i = 0; i < 5; i++) {
-            System.out.print(idx[i] + " ");
-        }
+        System.out.println(sb);
 
         br.close();
     }
