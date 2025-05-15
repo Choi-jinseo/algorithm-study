fc459ab1d4bdf6a39cf5076136885d1f6013ebf9
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-05-15 22:32:48 +0900

    [D3] Title: 구구단2, Time: 83 ms, Memory: 25,472 KB -BaekjoonHub

---
+++ SWEA/D3/12221. 구구단2/구구단2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            sb.append("#").append(t + 1).append(" ");
+            if (A >= 10 || B >= 10) sb.append(-1);
+            else sb.append(A * B);
+            sb.append('\n');
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
