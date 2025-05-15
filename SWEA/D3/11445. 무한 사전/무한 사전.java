0f94b3d0413a83d57711156e1dd8a060d6ae37cf
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-05-16 02:29:27 +0900

    [D3] Title: 무한 사전, Time: 72 ms, Memory: 24,320 KB -BaekjoonHub

--- SWEA/D3/11445. 무한 사전/무한 사전.java
+++ SWEA/D3/11445. 무한 사전/무한 사전.java
@@ -9,21 +9,8 @@ class Solution {
         for (int t = 0; t < T; t++) {
             String P = br.readLine();
             String Q = br.readLine();
-            if (P.length() >= Q.length()) sb.append("#").append(t + 1).append(" ").append('Y').append('\n');
-            else {
-                boolean flag = false;
-                for (int i = 0; i < P.length(); i++) {
-                    if (P.charAt(i) != Q.charAt(i)) {
-                        flag = true;
-                        break;
-                    }
-                }
-                if (flag) sb.append("#").append(t + 1).append(" ").append('Y').append('\n');
-                else {
-                    if (Q.length() - P.length() == 1 && Q.charAt(Q.length()-1) == 'a') sb.append("#").append(t + 1).append(" ").append('N').append('\n');
-                    else sb.append("#").append(t + 1).append(" ").append('Y').append('\n');
-                }
-            }
+            if (Q.equals(P + 'a')) sb.append("#").append(t + 1).append(" ").append('N').append('\n');
+            else sb.append("#").append(t + 1).append(" ").append('Y').append('\n');
         }
         System.out.print(sb);
         br.close();
