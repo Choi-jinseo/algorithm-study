d147468a7d251143407054729c506ef39e168f4d
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-05-18 03:08:54 +0900

    [D3] Title: 제곱 팰린드롬 수, Time: 76 ms, Memory: 25,216 KB -BaekjoonHub

--- SWEA/D3/10570. 제곱 팰린드롬 수/제곱 팰린드롬 수.java
+++ SWEA/D3/10570. 제곱 팰린드롬 수/제곱 팰린드롬 수.java
@@ -2,20 +2,13 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    static class Pair {
-        int N;
-        int rootN;
-        Pair(int N, int rootN) {
-            this.N = N;
-            this.rootN = rootN;
-        }
-    }
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        ArrayList<Pair> arr = new ArrayList<>();
-        for (int i = 1; i < Math.sqrt(1000); i++) {
-            arr.add(new Pair(i * i, i));
+        int[][] arr = new int[1001][2];
+        for (int i = 1; i <= Math.sqrt(1000); i++) {
+            arr[i*i][0] = 1; // 확인
+            arr[i*i][1] = i; // 제곱근 저장
         }
         int T = Integer.parseInt(br.readLine());
         for (int t = 0; t < T; t++) {
@@ -23,24 +16,25 @@ class Solution {
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
             int count = 0;
-            for (Pair p : arr) {
-                if (A <= p.N && p.N <= B) {
-                    String str1 = Integer.toString(p.N);
-                    String str2 = Integer.toString(p.rootN);
-                    boolean flag = false;
-                    for (int i = 0; i < str1.length() / 2; i++) {
-                        if (str1.charAt(i) != str1.charAt(str1.length() - 1 - i)) {
-                            flag = true;
+            for (int i = A; i <= B; i++) {
+                if (arr[i][0] == 1) { // 제곱수 확인
+                    String str1 = Integer.toString(i); // 제곱수
+                    String str2 = Integer.toString(arr[i][1]); // 제곱근
+                    boolean flag1 = false;
+                    boolean flag2 = false;
+                    for (int j = 0; j < str1.length() / 2; j++) {
+                        if (str1.charAt(j) != str1.charAt(str1.length() - 1 - j)) {
+                            flag1 = true;
                             break;
                         }
                     }
-                    for (int i = 0; i < str2.length() / 2; i++) {
-                        if (str2.charAt(i) != str2.charAt(str2.length() - 1 - i)) {
-                            flag = true;
+                    for (int j = 0; j < str2.length() / 2; j++) {
+                        if (str2.charAt(j) != str2.charAt(str2.length() - 1 - j)) {
+                            flag2 = true;
                             break;
                         }
                     }
-                    if (!flag) count++;
+                    if (!flag1 && !flag2) count++;
                 }
             }
             sb.append("#").append(t + 1).append(" ").append(count).append('\n');
