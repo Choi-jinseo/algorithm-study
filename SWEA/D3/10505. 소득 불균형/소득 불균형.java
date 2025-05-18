30a7e04041322454ee0971d8184a732c76e18e6f
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-05-18 22:21:41 +0900

    [D3] Title: 소득 불균형, Time: 116 ms, Memory: 32,256 KB -BaekjoonHub

---
+++ SWEA/D3/10505. 소득 불균형/소득 불균형.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            int[] arr = new int[N];
+            int sum = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+                sum += arr[i];
+            }
+            double abs = (double)sum / N;
+            int count = 0;
+            for (int i = 0; i < N; i++) {
+                if (arr[i] <= abs) count++;
+            }
+            sb.append("#").append(t + 1).append(" ").append(count).append('\n');
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
