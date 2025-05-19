9b4ef7a86b9d1ca593234132a57b0ec481425113
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-05-19 21:05:08 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 74 ms, Memory: 25,344 KB -BaekjoonHub

---
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            String str = br.readLine();
+            int count = 0;
+            char bit = '0';
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == bit) continue;
+                else {
+                    count++;
+                    bit = str.charAt(i);
+                }
+            }
+            sb.append("#").append(t + 1).append(" ").append(count).append('\n');
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
