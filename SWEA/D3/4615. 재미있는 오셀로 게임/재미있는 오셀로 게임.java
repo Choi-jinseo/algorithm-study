c430d3adbe3ade31e530f34e840f64664b68fd40
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-05-20 01:45:54 +0900

    [D3] Title: 재미있는 오셀로 게임, Time: 95 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.java
+++ SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.java
@@ -2,6 +2,16 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
+    static int[][] d = {
+            {-1,0},
+            {1,0},
+            {0,-1},
+            {0,1},
+            {-1,-1},
+            {1,1},
+            {-1,1},
+            {1,-1}
+    };
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -18,132 +28,32 @@ class Solution {
             arr[N / 2][N / 2] = 2;
             for (int i = 0; i < M; i++) {
                 st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken()) - 1;
-                int y = Integer.parseInt(st.nextToken()) - 1;
-                arr[x][y] = Integer.parseInt(st.nextToken());
+                int x0 = Integer.parseInt(st.nextToken()) - 1;
+                int y0 = Integer.parseInt(st.nextToken()) - 1;
+                arr[x0][y0] = Integer.parseInt(st.nextToken());
                 // 8방 탐색
-                // 상하
-                if (x > 1 && arr[x-1][y] != arr[x][y]) {
-                    int count = 0;
-                    boolean flag = false;
-                    for (int d = 1; x-d >= 0; d++) {
-                        if (arr[x-d][y] == 0) break;
-                        else if (arr[x-d][y] == arr[x][y]) {
-                            flag = true; // 변경 조건
-                            break;
+                for (int j = 0; j < 8; j++) {
+                    int x1 = x0 + d[j][0];
+                    int y1 = y0 + d[j][1];
+                    int x2 = x1 + d[j][0];
+                    int y2 = y1 + d[j][1];
+                    if (x2 >= 0 && x2 < N && y2 >= 0 && y2 < N && arr[x1][y1] != arr[x0][y0] && arr[x1][y1] != 0) {
+                        int count = 0;
+                        boolean change = false;
+                        for (int xt = x1, yt = y1; xt >= 0 && xt < N && yt >= 0 && yt < N; xt += d[j][0], yt += d[j][1]) {
+                            if (arr[xt][yt] == arr[x1][y1]) count++;
+                            else if (arr[xt][yt] == arr[x0][y0]) {
+                                change = true;
+                                break;
+                            }
+                            else break;
                         }
-                        else count++;
-                    }
-                    if (flag) for (int d = 1; d < 1 + count; d++) {
-                        arr[x-d][y] = arr[x][y];
-                    }
-                }
-                if (x < N - 2 && arr[x+1][y] != arr[x][y]) {
-                    int count = 0;
-                    boolean flag = false;
-                    for (int d = 1; x+d < N; d++) {
-                        if (arr[x+d][y] == 0) break;
-                        else if (arr[x+d][y] == arr[x][y]) {
-                            flag = true; // 변경 조건
-                            break;
+                        if (change) {
+                            for (int xt = x1, yt = y1; count > 0; xt += d[j][0], yt += d[j][1]) {
+                                arr[xt][yt] = arr[x0][y0];
+                                count--;
+                            }
                         }
-                        else count++;
-                    }
-                    if (flag) for (int d = 1; d < 1 + count; d++) {
-                        arr[x+d][y] = arr[x][y];
-                    }
-                }
-                // 좌우
-                if (y > 1 && arr[x][y-1] != arr[x][y]) {
-                    int count = 0;
-                    boolean flag = false;
-                    for (int d = 1; y-d >= 0; d++) {
-                        if (arr[x][y-d] == 0) break;
-                        else if (arr[x][y-d] == arr[x][y]) {
-                            flag = true; // 변경 조건
-                            break;
-                        }
-                        else count++;
-                    }
-                    if (flag) for (int d = 1; d < 1 + count; d++) {
-                        arr[x][y-d] = arr[x][y];
-                    }
-                }
-                if (y < N - 2 && arr[x][y+1] != arr[x][y]) {
-                    int count = 0;
-                    boolean flag = false;
-                    for (int d = 1; y+d < N; d++) {
-                        if (arr[x][y+d] == 0) break;
-                        else if (arr[x][y+d] == arr[x][y]) {
-                            flag = true; // 변경 조건
-                            break;
-                        }
-                        else count++;
-                    }
-                    if (flag) for (int d = 1; d < 1 + count; d++) {
-                        arr[x][y+d] = arr[x][y];
-                    }
-                }
-                // \
-                if (x > 1 && y > 1 && arr[x-1][y-1] != arr[x][y]) {
-                    int count = 0;
-                    boolean flag = false;
-                    for (int d = 1; x-d >= 0 && y-d >= 0; d++) {
-                        if (arr[x-d][y-d] == 0) break;
-                        else if (arr[x-d][y-d] == arr[x][y]) {
-                            flag = true; // 변경 조건
-                            break;
-                        }
-                        else count++;
-                    }
-                    if (flag) for (int d = 1; d < 1 + count; d++) {
-                        arr[x-d][y-d] = arr[x][y];
-                    }
-                }
-                if (x < N - 2 && y < N - 2 && arr[x+1][y+1] != arr[x][y]) {
-                    int count = 0;
-                    boolean flag = false;
-                    for (int d = 1; x+d < N && y+d < N; d++) {
-                        if (arr[x+d][y+d] == 0) break;
-                        else if (arr[x+d][y+d] == arr[x][y]) {
-                            flag = true; // 변경 조건
-                            break;
-                        }
-                        else count++;
-                    }
-                    if (flag) for (int d = 1; d < 1 + count; d++) {
-                        arr[x+d][y+d] = arr[x][y];
-                    }
-                }
-                // /
-                if (x > 1 && y < N - 2 && arr[x-1][y+1] != arr[x][y]) {
-                    int count = 0;
-                    boolean flag = false;
-                    for (int d = 1; x-d >= 0 && y+d < N; d++) {
-                        if (arr[x-d][y+d] == 0) break;
-                        else if (arr[x-d][y+d] == arr[x][y]) {
-                            flag = true; // 변경 조건
-                            break;
-                        }
-                        else count++;
-                    }
-                    if (flag) for (int d = 1; d < 1 + count; d++) {
-                        arr[x-d][y+d] = arr[x][y];
-                    }
-                }
-                if (x < N - 2 && y > 1 && arr[x+1][y-1] != arr[x][y]) {
-                    int count = 0;
-                    boolean flag = false;
-                    for (int d = 1; x+d < N && y-d >= 0; d++) {
-                        if (arr[x+d][y-d] == 0) break;
-                        else if (arr[x+d][y-d] == arr[x][y]) {
-                            flag = true; // 변경 조건
-                            break;
-                        }
-                        else count++;
-                    }
-                    if (flag) for (int d = 1; d < 1 + count; d++) {
-                        arr[x+d][y-d] = arr[x][y];
                     }
                 }
             }
