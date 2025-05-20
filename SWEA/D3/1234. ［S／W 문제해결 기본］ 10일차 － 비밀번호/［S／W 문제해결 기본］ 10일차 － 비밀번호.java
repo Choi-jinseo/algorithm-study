92d55c18e99cfcd622e84ac062a93328e8ddbad2
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-05-20 22:58:42 +0900

    [D3] Title: [S/W 문제해결 기본] 10일차 - 비밀번호, Time: 76 ms, Memory: 25,344 KB -BaekjoonHub

---
+++ SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = 10;
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            ArrayList<Integer> arr = new ArrayList<>();
+            String str = st.nextToken();
+            for (int i = 0; i < N; i++) {
+                arr.add(str.charAt(i) - '0');
+            }
+            boolean flag = false;
+            while (!flag) {
+                flag = true;
+                for (int i = 0; i < arr.size() - 1; i++) {
+                    if (arr.get(i) == arr.get(i+1)) {
+                        flag = false;
+                        arr.remove(i+1);
+                        arr.remove(i);
+                    }
+                }
+            }
+            String answer = "";
+            for (int i : arr) {
+                answer += i;
+            }
+            sb.append("#").append(t+1).append(" ").append(answer).append('\n');
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
