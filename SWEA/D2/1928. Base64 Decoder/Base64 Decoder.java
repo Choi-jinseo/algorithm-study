8d439d2d2902843c2f2764271510b35239584b28
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-05-21 22:37:17 +0900

    [D2] Title: Base64 Decoder, Time: 74 ms, Memory: 22,144 KB -BaekjoonHub

--- SWEA/D2/1928. Base64 Decoder/Base64 Decoder.java
+++ SWEA/D2/1928. Base64 Decoder/Base64 Decoder.java
@@ -1,42 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Solution {
-    public static void main(String[] args) throws IOException {
+class Solution {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
         for (int t = 0; t < T; t++) {
             String str = br.readLine();
             int[] buffer = new int[str.length() * 6];
-            int cursor = 0;
-            // buffer 을 8로 끊으면서 int 로 변환하고 (char)65 <-- 이렇게 된놈을 sb.append()하기
             for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
                 int value = 0;
-                if ('A' <= str.charAt(i) && str.charAt(i) <= 'Z') value = str.charAt(i) - 'A';
-                else if ('a' <= str.charAt(i) && str.charAt(i) <= 'z') value = str.charAt(i) - 'a' + 26;
-                else if ('0' <= str.charAt(i) && str.charAt(i) <= '9') value = str.charAt(i) - '0' + 52;
-                else if (str.charAt(i) == '+') value = 62;
-                else if (str.charAt(i) == '/') value = 63;
-                for (int j = 0; j < 6; j++) {
-                    if (value >= Math.pow(2, 5-j)) {
-                        buffer[cursor + j] = 1;
-                        value -= Math.pow(2, 5-j);
-                    }
-                    else buffer[cursor + j] = 0;
+                if ('A' <= c && c <= 'Z') value = c - 'A';
+                else if ('a' <= c && c <= 'z') value = c - 'a' + 26;
+                else if ('0' <= c && c <= '9') value = c - '0' + 52;
+                else if (c == '+') value = 62;
+                else if (c == '/') value = 63;
+                if (value >= 32) {
+                    buffer[i * 6] = 1;
+                    value -= 32;
                 }
-                cursor += 6;
+                if (value >= 16) {
+                    buffer[i * 6 + 1] = 1;
+                    value -= 16;
+                }
+                if (value >= 8) {
+                    buffer[i * 6 + 2] = 1;
+                    value -= 8;
+                }
+                if (value >= 4) {
+                    buffer[i * 6 + 3] = 1;
+                    value -= 4;
+                }
+                if (value >= 2) {
+                    buffer[i * 6 + 4] = 1;
+                    value -= 2;
+                }
+                if (value >= 1) {
+                    buffer[i * 6 + 5] = 1;
+                }
             }
-            sb.append("#").append(t+1).append(" ");
+            sb.append("#").append(t + 1).append(" ");
             for (int i = 0; i < buffer.length; i+=8) {
-                int value = 0;
-                for (int j = 0; j < 8; j++) {
-                    if (buffer[i+j] == 1) value += Math.pow(2, 7-j);
-                }
-                sb.append((char)value);
+                char sum = 0;
+                if (buffer[i] == 1) sum += 128;
+                if (buffer[i+1] == 1) sum += 64;
+                if (buffer[i+2] == 1) sum += 32;
+                if (buffer[i+3] == 1) sum += 16;
+                if (buffer[i+4] == 1) sum += 8;
+                if (buffer[i+5] == 1) sum += 4;
+                if (buffer[i+6] == 1) sum += 2;
+                if (buffer[i+7] == 1) sum += 1;
+                sb.append(sum);
             }
             sb.append('\n');
         }
