e50009d9e7bd47e61e3ae90a9ccc43e293a98ce3
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-05-21 22:45:50 +0900

    [D2] Title: Base64 Decoder, Time: 73 ms, Memory: 22,144 KB -BaekjoonHub

--- SWEA/D2/1928. Base64 Decoder/Base64 Decoder.java
+++ SWEA/D2/1928. Base64 Decoder/Base64 Decoder.java
@@ -17,41 +17,19 @@ class Solution {
                 else if ('0' <= c && c <= '9') value = c - '0' + 52;
                 else if (c == '+') value = 62;
                 else if (c == '/') value = 63;
-                if (value >= 32) {
-                    buffer[i * 6] = 1;
-                    value -= 32;
-                }
-                if (value >= 16) {
-                    buffer[i * 6 + 1] = 1;
-                    value -= 16;
-                }
-                if (value >= 8) {
-                    buffer[i * 6 + 2] = 1;
-                    value -= 8;
-                }
-                if (value >= 4) {
-                    buffer[i * 6 + 3] = 1;
-                    value -= 4;
-                }
-                if (value >= 2) {
-                    buffer[i * 6 + 4] = 1;
-                    value -= 2;
-                }
-                if (value >= 1) {
-                    buffer[i * 6 + 5] = 1;
+                for (int j = 0; j < 6; j++) {
+                    if (value >= Math.pow(2, 5 - j)) {
+                        buffer[i * 6 + j] = 1;
+                        value -= Math.pow(2, 5 - j);
+                    }
                 }
             }
             sb.append("#").append(t + 1).append(" ");
             for (int i = 0; i < buffer.length; i+=8) {
                 char sum = 0;
-                if (buffer[i] == 1) sum += 128;
-                if (buffer[i+1] == 1) sum += 64;
-                if (buffer[i+2] == 1) sum += 32;
-                if (buffer[i+3] == 1) sum += 16;
-                if (buffer[i+4] == 1) sum += 8;
-                if (buffer[i+5] == 1) sum += 4;
-                if (buffer[i+6] == 1) sum += 2;
-                if (buffer[i+7] == 1) sum += 1;
+                for (int j = 0; j < 8; j++) {
+                    if (buffer[i+j] == 1) sum += Math.pow(2, 7-j);
+                }
                 sb.append(sum);
             }
             sb.append('\n');
