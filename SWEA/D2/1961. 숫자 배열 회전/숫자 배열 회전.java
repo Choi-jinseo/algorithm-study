4208bdf0ba8960eb8661b954090568284151d859
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-05-22 01:29:44 +0900

    [D2] Title: 숫자 배열 회전, Time: 73 ms, Memory: 24,064 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.java
@@ -1,41 +1,39 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-class Solution
-{
-    public static void main(String args[]) throws Exception
-    {
+class Solution {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
         for (int t = 0; t < T; t++) {
             int N = Integer.parseInt(br.readLine());
-            int[][] arr0 = new int[N][N];
+            int[][] arr = new int[N][N];
             for (int i = 0; i < N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
-                    arr0[i][j] = Integer.parseInt(st.nextToken());
+                    arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
             int[][] arr90 = new int[N][N];
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    arr90[i][j] = arr0[N-j-1][i];
+                    arr90[i][j] = arr[N - 1 - j][i];
                 }
             }
             int[][] arr180 = new int[N][N];
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    arr180[i][j] = arr90[N-j-1][i];
+                    arr180[i][j] = arr90[N - 1 - j][i];
                 }
             }
             int[][] arr270 = new int[N][N];
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    arr270[i][j] = arr180[N-j-1][i];
+                    arr270[i][j] = arr180[N - 1 - j][i];
                 }
             }
-            sb.append("#").append(t+1).append(" ").append('\n');
+            sb.append("#").append(t + 1).append('\n');
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     sb.append(arr90[i][j]);
