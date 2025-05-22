d3967d1ffc4da523d2f742c149b0c05699f9d613
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-05-23 00:59:12 +0900

    [D1] Title: 평균값 구하기, Time: 74 ms, Memory: 25,472 KB -BaekjoonHub

---
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int sum = 0;
+            for (int i = 0; i < 10; i++) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+            if ((double)sum / 10 >= sum / 10 + 0.5) sum = sum / 10 + 1;
+            else sum = sum / 10;
+            sb.append("#").append(t+1).append(" ").append(sum).append('\n');
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
