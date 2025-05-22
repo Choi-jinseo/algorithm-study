ad70dcf176cb3521ddd22cf297451570813ab09e
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-05-22 19:05:54 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 777 ms, Memory: 164,248 KB -BaekjoonHub

---
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            int max = -1;
+            for (int i = 0; i < N-1; i++) { // 첫번째 수
+                for (int j = i+1; j < N; j++) { // 두번째 수
+                    String str = Integer.toString(arr[i] * arr[j]);
+                    boolean flag = false;
+                    for (int k = 0; k < str.length()-1; k++) {
+                        if (str.charAt(k) > str.charAt(k+1)) {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (!flag) { // 단조 증가 수
+                        max = Math.max(max, arr[i] * arr[j]);
+                    }
+                }
+            }
+            sb.append("#").append(t+1).append(" ").append(max).append('\n');
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
