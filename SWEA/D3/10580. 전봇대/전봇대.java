ce3f2e56bfba11d04fbfc7bc4f3268e29f1c0eda
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-05-24 14:03:46 +0900

    [D3] Title: 전봇대, Time: 203 ms, Memory: 38,876 KB -BaekjoonHub

--- SWEA/D3/10580. 전봇대/전봇대.java
+++ SWEA/D3/10580. 전봇대/전봇대.java
@@ -2,24 +2,26 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
+    static int[] arr;
+    static int max;
+    static int sum;
+    static int min = 1;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         int T = Integer.parseInt(br.readLine());
         for (int t = 0; t < T; t++) {
             int N = Integer.parseInt(br.readLine());
-            int[] arr = new int[N];
-            int[] brr = new int[N];
+            int[][] arr = new int[N][2];
             for (int i = 0; i < N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                arr[i] = Integer.parseInt(st.nextToken());
-                brr[i] = Integer.parseInt(st.nextToken());
+                arr[i][0] = Integer.parseInt(st.nextToken());
+                arr[i][1] = Integer.parseInt(st.nextToken());
             }
             int count = 0;
-            for (int i = 0; i < N - 1; i++) {
-                for (int j = i + 1; j < N; j++) {
-                    if ((arr[i] < arr[j] && brr[i] > brr[j]) || (arr[i] > arr[j] && brr[i] < brr[j])) {
+            for (int i = 0; i < N-1; i++) {
+                for (int j = i+1; j < N; j++) {
+                    if ((arr[i][0] < arr[j][0] && arr[i][1] > arr[j][1]) || (arr[i][0] > arr[j][0] && arr[i][1] < arr[j][1])) {
                         count++;
                     }
                 }
