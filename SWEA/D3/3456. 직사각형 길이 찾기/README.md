8c6bafc32c4b8291219c4ca65a7e3d521d99ca59
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-06-20 23:34:48 +0900

    [D3] Title: 직사각형 길이 찾기, Time: 76 ms, Memory: 25,600 KB -BaekjoonHub

---
+++ SWEA/D3/3456. 직사각형 길이 찾기/README.md
@@ -0,0 +1,15 @@
+# [D3] 직사각형 길이 찾기 - 3456 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWFPmsqqALwDFAV0) 
+
+### 성능 요약
+
+메모리: 25,600 KB, 시간: 76 ms, 코드길이: 978 Bytes
+
+### 제출 일자
+
+2025-06-20 23:34
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/3456. 직사각형 길이 찾기/직사각형 길이 찾기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int answer = 0;
+            if (a==b && b==c) answer = a;
+            else {
+                if (a == b) answer = c;
+                else {
+                    if (a==c) answer = b;
+                    else answer = a;
+                }
+            }
+            sb.append("#").append(t + 1).append(" ").append(answer).append('\n');
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
