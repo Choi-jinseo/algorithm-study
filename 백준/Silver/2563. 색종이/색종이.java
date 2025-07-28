fac8cf918c0817b75ce0462c51871c841f727143
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-07-29 00:13:18 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -4,18 +4,25 @@ class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine()); // 색종이 수
-        int[][] arr = new int[100][100];
+        boolean[][] arr = new boolean[100][100];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken()); // 색종이 시작 x좌표
             int y = Integer.parseInt(st.nextToken()); // 색종이 시작 y좌표
             for (int j = x; j < x + 10; j++) {
                 for (int k = y; k < y + 10; k++) {
-                    arr[j][k] = 1;
+                    arr[j][k] = true;
                 }
             }
         }
-        System.out.println(Arrays.stream(arr).flatMapToInt(row -> Arrays.stream(row)).sum());
+        int count = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (arr[i][j]) count++;
+            }
+        }
+        System.out.println(count);
+        
         br.close();
     }
 }
