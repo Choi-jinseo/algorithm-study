eeab936f7fd8ec612bd4cf7e235ce6f1303663ef
최진서 <83263721+Choi-jinseo@users.noreply.github.com>
2025-09-09 23:47:39 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 576 ms, Memory: 93184 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] len = new int[N+1];
+        int idx = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (len[idx] < num) {
+                len[++idx] = num;
+            } else {
+                // 이분탐색
+                int start = 0;
+                int end = idx;
+                while (start < end) {
+                    int mid = (start + end) / 2;
+                    if (len[mid] < num) {
+                        start = mid+1;
+                    } else {
+                        end = mid;
+                    }
+                }
+                len[end] = num;
+            }
+        }
+        System.out.println(idx);
+        br.close();
+    }
+}
